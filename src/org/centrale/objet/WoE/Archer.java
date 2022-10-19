84d198fe934cc5bfe56f252b0f3f7f7d08a5cf8c
Edwin <edwinhdezrocha@outlook.com>
2022-10-19 14:12:45 +0200

    Space

--- src/org/centrale/objet/WoE/Archer.java
+++ src/org/centrale/objet/WoE/Archer.java
@@ -38,6 +38,7 @@ public class Archer extends Personnage implements Combattant {
     /**
      * Constructeur de recopie
      *
+     *
      * @param a : Objet du type Archer qui sera copié
      */
     public Archer(Archer a) {
